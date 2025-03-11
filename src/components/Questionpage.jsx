a2ce0c43ae3244d281d44e2442ddf7041175ee90
Anshul Yadav <ay839990@gmail.com>
2025-03-11 15:32:12 +0530

    commits

--- src/components/Questionpage.jsx
+++ src/components/Questionpage.jsx
@@ -1,21 +1,10 @@
 import "../App.css";
 
-
 function Questionpage() {
-  
-
   return (
     <div className="ques-container">
-     <img
-        src="/plant1.png" 
-        alt="plant"
-        className="plant1-button"
-      />
-      <img
-        src="/plant1.png" 
-        alt="plant"
-        className="plant2-button"
-      />
+      <img src="/plant1.png" alt="plant" className="plant1-button" />
+      <img src="/plant1.png" alt="plant" className="plant2-button" />
       <img
         src="/true.png" // ✅ Remove 'public/'
         alt="Help"
@@ -26,7 +15,36 @@ function Questionpage() {
         alt="Help"
         className="false-button"
       />
-      
+      <img
+        src="/gr1.png" // ✅ Remove 'public/'
+        alt="Help"
+        className="gr1-button"
+      />
+      <img
+        src="/gr2.png" // ✅ Remove 'public/'
+        alt="Help"
+        className="gr2-button"
+      />
+      <img
+        src="/gr3.png" // ✅ Remove 'public/'
+        alt="Help"
+        className="gr3-button"
+      />
+      <img
+        src="/rr1.png" // ✅ Remove 'public/'
+        alt="Help"
+        className="rr1-button"
+      />
+      <img
+        src="/rr2.png" // ✅ Remove 'public/'
+        alt="Help"
+        className="rr2-button"
+      />
+      <img
+        src="/rr3.png" // ✅ Remove 'public/'
+        alt="Help"
+        className="rr3-button"
+      />
     </div>
   );
 }
